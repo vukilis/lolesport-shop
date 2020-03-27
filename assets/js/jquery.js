427f7f5dff3d8fa12331a73168b3c5784490bc51
vukilis <58575807+vukilis@users.noreply.github.com>
2020-03-27 20:02:15 +0100

    Update jquery.js

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,12 +1,24 @@
 //preload
-$(window).on('load', function () {
-  if ($('#preloader').length) {
-    $('#preloader').delay(1000).fadeOut('slow', function () {
-			$(this).remove();
-    });
-  }
-});
-
+// $(window).on('load', function () {
+//   if ($('#preloader').length) {
+//     $('#preloader').delay(1000).fadeOut('slow', function () {
+// 			$(this).remove();
+//     });
+//   }
+// });
+document.onreadystatechange = function() {
+     if (document.readyState !== "complete") {
+         document.querySelector(
+           "body").style.visibility = "hidden";
+         document.querySelector(
+           "#preloader").style.visibility = "visible";
+     } else {
+         document.querySelector(
+           "#preloader").style.display = "none";
+         document.querySelector(
+           "body").style.visibility = "visible";
+     }
+ };
 
 // aktivan on scroll
 $(window).scroll(function() {
